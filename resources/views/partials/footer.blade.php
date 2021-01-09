ae7f4f340442384aecaac358846e4ee7c4961724
Manuel <manuel.devito@icloud.com>
2021-01-09 14:37:48 +0100

    Aggiunta Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,142 @@
-
+<footer>
+    <div id="sezione-footer-top">
+        <div class="contenitore">
+            <div class="footer-logo">
+                <div class="footer-contenitore-menu">
+                    <div class="footer-menu">
+                        <h3 class="footer-menu-title">DC COMICS</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+                        <h3 class="footer-menu-title shop">SHOP</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-menu">
+                        <h3 class="footer-menu-title">DC</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Terms Of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-menu">
+                        <h3 class="footer-menu-title">SITES</h3>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+                    <p>
+                        <a href="#">Cookies Settings</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="contenitore">
+                <div class="footer-bottom-contenitore">
+                    <div class="footer-left">
+                        <a href="#" class="button">
+                            Sign-up now!
+                        </a>
+                    </div>
+                    <div class="footer-right">
+                        <div class="social">
+                            <h2 class="social-title">Follow us</h2>
+                            <ul>
+                                <li class="facebook">
+                                    <a href="#"></a>
+                                </li>
+                                <li class="twitter">
+                                    <a href="#"></a>
+                                </li>
+                                <li class="youtube">
+                                    <a href="#"></a>
+                                </li>
+                                <li class="pinterest">
+                                    <a href="#"></a>
+                                </li>
+                                <li class="periscope">
+                                    <a href="#"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
