f462e418202d205f96a9c6eedcd57fda718ba933
Manuel <manuel.devito@icloud.com>
2021-01-10 00:53:18 +0100

    Fine pagina dettaglio

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -18,7 +18,7 @@
 </div>
 </section>
 <section>
-<div class="sezione-text-comic">
+<div id="sezione-text-comic">
     <div class="small-contenitore">
         <div class="contenitore-text-img">
             <div class="contenitore-left">
@@ -44,6 +44,86 @@
         </div>
     </div>
 </div>
+</section>
+<section>
+    <div id="sezione-artisti">
+        <div class="small-contenitore">
+            <div class="contenitore-sezione-artisti">
+                <div class="sezione-artisti-left margin">
+                    <h2>Talent</h2>
+                    <ul>
+                        <li>
+                            <span>Art by:</span>
+                            <span>
+                                @foreach ($comic['artists'] as $single_artist)
+                                    <a href="#">{{ $single_artist }}</a>{{ !$loop->last ? ',' : '' }}
+                                @endforeach
+                            </span>
+                        </li>
+                        <li class="riga">
+                            <span>Written by:</span>
+                            <span>
+                                @foreach ($comic['writers'] as $single_writer)
+                                    <a href="#">{{ $single_writer }}</a>{{ !$loop->last ? ',' : '' }}
+                                @endforeach
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="sezione-artisti-right">
+                    <h2>Specs</h2>
+                    <ul>
+                        <li>
+                            <span>Series:</span>
+                            <span>
+                                <a class="name-series" href="#">
+                                    {{ $comic['series']}}
+                                </a>
+                            </span>
+                        </li>
+                        <li>
+                            <span>U.S. Price:</span>
+                            <span>{{ $comic['price']}}</span>
+                        </li>
+                        <li>
+                            <span>On Sale Date:</span>
+                            <span>{{ DateTime::createFromFormat('Y-m-d', $comic['sale_date'])->format('M d Y') }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="sezione-contenitore-blocchi">
+            <div class="small-contenitore">
+                <div class="contenitore-blocchi">
+                    <div class="blocchi">
+                        <a href="#">
+                            <span class="text-icon">Digital Comics</span>
+                            <span class="icon"></span>
+                        </a>
+                    </div>
+                    <div class="blocchi">
+                        <a href="#">
+                            <span class="text-icon">Shop Dc</span>
+                            <span class="icon"></span>
+                        </a>
+                    </div>
+                    <div class="blocchi">
+                        <a href="#">
+                            <span class="text-icon">Comic Shop Locator</span>
+                            <span class="icon"></span>
+                        </a>
+                    </div>
+                    <div class="blocchi">
+                        <a href="#">
+                            <span class="text-icon">Subscriptions</span>
+                            <span class="icon"></span>
+                        </a>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
 </section>
 
 @include('partials.section-icon')
